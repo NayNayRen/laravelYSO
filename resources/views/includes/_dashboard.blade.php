333a4d88d6b72f1b521bbdfb8b36ee64232f9af5
Nate M <jeditendencies35@gmail.com>
2022-06-17 14:34:56 -0400

    continued with the dashboard style and movements

--- resources/views/includes/_dashboard.blade.php
+++ resources/views/includes/_dashboard.blade.php
@@ -2,32 +2,40 @@
     <div class="dashboard-content">
         <span id="dashboard-close-button" class="dashboard-close-button">Close
         </span>
-        <span id="dashboard-user-preferences-button" class="dashboard-user-preferences-button"><i class="fa fa-bars"
-                aria-hidden="true"></i>
+        <span id="dashboard-user-preferences-button" class="dashboard-user-preferences-button"><i
+                class="fa fa-ellipsis-h" aria-hidden="true"></i>
         </span>
         @auth
             <div class="dashboard-user-container">
-                <div>User Preferences</div>
+                <span>User Preferences</span>
                 <div class="registered-user-profile">
                     <img src="{{ asset('img/male-profile.png') }}"
                         class="registered-user-profile-picture" alt="Profile Picture">
                 </div>
-                <div>Welcome {{ ucfirst(auth()->user()->firstName) }}</div>
+                <div>Logged in as:</div>
+                <span>{{ ucfirst(auth()->user()->firstName) }}</span>
+                <div>Location:</div>
+                <span>{{ auth()->user()->email }}</span>
             </div>
             <div class="dashboard-right-container">
-                {{-- <span>Right Side Container</span> --}}
+                <h4>My Coupons</h4>
+                <h4>My Categories</h4>
             </div>
         @else
             <div class="dashboard-user-container">
-                <div>User Preferences</div>
+                <span>User Preferences</span>
                 <div class="registered-user-profile">
                     <img src="{{ asset('img/male-profile.png') }}"
                         class="registered-user-profile-picture" alt="Profile Picture">
                 </div>
-                <div>Hello Guest</div>
+                <div>Logged in as:</div>
+                <span>Guest</span>
+                <div>Location:</div>
+                <span>N/A</span>
             </div>
             <div class="dashboard-right-container">
-                {{-- <span>Right Side Container</span> --}}
+                <h4>My Coupons</h4>
+                <h4>My Categories</h4>
             </div>
         @endauth
     </div>
