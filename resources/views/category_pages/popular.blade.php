6e95c9a0775034611bcafe0f9b389a789b7b8859
Nate M <jeditendencies35@gmail.com>
2022-06-21 15:14:51 -0400

    sourced, sized, added and styled tech and popular pages

--- resources/views/category_pages/popular.blade.php
+++ resources/views/category_pages/popular.blade.php
@@ -1,13 +1,84 @@
 @include('includes._header')
 <div class="main">
-    <div class="container">
+    <div class="banner">
+        <div class="banner-slide-container">
+            {{-- SLIDE 1 --}}
+            <div class="banner-slide">
+                <div class="banner-gradient"></div>
+                <div class="banner-container-text">
+                    <span class='banner-text-one'>free round</span>
+                    <span class='banner-text-two'>for you and a guest after initial purchase.</span>
+                    <a href="#" class="banner-redemption">Get Deal Now!</a>
+                </div>
+                <div class="banner-container-image banner-image-popular-1">
+                </div>
+                <img class="banner-logo" src="{{ asset('img/fun/congo-logo.png') }}"
+                    alt="Micro Center Company Logo">
+            </div>
+            {{-- SLIDE 2 --}}
+            <div class="banner-slide even">
+                <img class="banner-logo" src="{{ asset('img/fun/amc-logo.png') }}"
+                    alt="Newegg Company Logo">
+                <div class="banner-gradient"></div>
+                <div class="banner-container-image banner-image-popular-2">
+                </div>
+                <div class="banner-container-text">
+                    <span class='banner-text-one'>on demand</span>
+                    <span class='banner-text-two'>with 20% lower rates.</span>
+                    <a href="#" class="banner-redemption">Get Deal Now!</a>
+                </div>
+            </div>
+            {{-- SLIDE 3 --}}
+            <div class="banner-slide">
+                <div class="banner-gradient"></div>
+                <div class="banner-container-text">
+                    <span class='banner-text-one'>free wipers</span>
+                    <span class='banner-text-two'>with purchase of $50 or more.</span>
+                    <a href="#" class="banner-redemption">Get Deal Now!</a>
+                </div>
+                <div class="banner-container-image banner-image-popular-3">
+                </div>
+                <img class="banner-logo" src="{{ asset('img/auto/advanced-auto-logo.png') }}"
+                    alt="Best Buy Logo">
+            </div>
+            {{-- SLIDE 4 --}}
+            <div class="banner-slide even">
+                <img class="banner-logo" src="{{ asset('img/auto/sw-logo2.png') }}"
+                    alt="Apple Store Company Logo">
+                <div class="banner-gradient"></div>
+                <div class="banner-container-image banner-image-popular-4">
+                </div>
+                <div class="banner-container-text">
+                    <span class='banner-text-one'>2nd color</span>
+                    <span class='banner-text-two'>free with first mixs.</span>
+                    <a href="#" class="banner-redemption">Get Deal Now!</a>
+                </div>
+            </div>
+        </div>
+        {{-- BANNER ARROWS --}}
+        <div class="banner-arrows banner-arrows-alternate">
+            <span id='prev'><i class="fa fa-arrow-left" aria-hidden="true"></i></span>
+            <span id='next'><i class="fa fa-arrow-right" aria-hidden="true"></i></span>
+        </div>
+    </div>
+    {{-- MAIN CONTENT CONTAINER --}}
+    <div class="view-all-container-heading">
+        <button id="dashboard-open-button" class="user-icon view-all-user-icon"><i class="fa fa-user"
+                aria-hidden="true"></i></button>
+        <h1>Our most popular selections.</h1>
+        <h3>Used most, because they save you the most.</h3>
+        {{-- HIDDEN DASHBOARD --}}
+        @include('includes._dashboard')
+    </div>
+    <div class="container view-all">
         <div class="container-left">
             <span class="category-heading">Most Popular</span>
-        </div>
-        {{-- MAIN CONTENT CONTAINER --}}
-        <div class="container-right">
             {{-- CUSTOM PAGE ARROWS --}}
-            {{ $deals->links('vendor.pagination.custom-pagination') }}
+            <div class="view-all-arrow-container">
+                {{ $deals->links('vendor.pagination.custom-view-all-pagination') }}
+            </div>
+        </div>
+        <div class="container-right">
             <div class="card-display-view-all">
                 @foreach($deals as $deal)
                     {{-- CARD BLOCK --}}
@@ -40,4 +111,7 @@
         </div>
     </div>
 </div>
+{{-- PAGE SPECIFIC SCRIPTS --}}
+<script src="{{ asset('js/scrolling-banner.js') }}"></script>
+<script src="{{ asset('js/show-dashboard.js') }}"></script>
 @include('includes._footer')
--- resources/views/category_pages/tech.blade.php
+++ resources/views/category_pages/tech.blade.php
@@ -6,8 +6,8 @@
             <div class="banner-slide">
                 <div class="banner-gradient"></div>
                 <div class="banner-container-text">
-                    <span class='banner-text-one'>free!!!</span>
-                    <span class='banner-text-two'>stuffed crust with next delivery.</span>
+                    <span class='banner-text-one'>early deal</span>
+                    <span class='banner-text-two'>notices when you join our mailing list.</span>
                     <a href="#" class="banner-redemption">Get Deal Now!</a>
                 </div>
                 <div class="banner-container-image banner-image-tech-1">
@@ -24,8 +24,8 @@
                 <div class="banner-container-image banner-image-tech-2">
                 </div>
                 <div class="banner-container-text">
-                    <span class='banner-text-one'>buy 10!!!</span>
-                    <span class='banner-text-two'>get 10, bone or boneless.</span>
+                    <span class='banner-text-one'>free shipping!!!</span>
+                    <span class='banner-text-two'>on orders of $75 or more.</span>
                     <a href="#" class="banner-redemption">Get Deal Now!</a>
                 </div>
             </div>
@@ -33,8 +33,8 @@
             <div class="banner-slide">
                 <div class="banner-gradient"></div>
                 <div class="banner-container-text">
-                    <span class='banner-text-one'>$5 sub</span>
-                    <span class='banner-text-two'>with purchase of one footlong.</span>
+                    <span class='banner-text-one'>20% off</span>
+                    <span class='banner-text-two'>first use of geek squad.</span>
                     <a href="#" class="banner-redemption">Get Deal Now!</a>
                 </div>
                 <div class="banner-container-image banner-image-tech-3">
@@ -50,8 +50,8 @@
                 <div class="banner-container-image banner-image-tech-4">
                 </div>
                 <div class="banner-container-text">
-                    <span class='banner-text-one'>2 for $5</span>
-                    <span class='banner-text-two'>mix from a limited menu.</span>
+                    <span class='banner-text-one'>discounted</span>
+                    <span class='banner-text-two'>rates with in store repairs.</span>
                     <a href="#" class="banner-redemption">Get Deal Now!</a>
                 </div>
             </div>
@@ -79,7 +79,6 @@
                 {{ $deals->links('vendor.pagination.custom-view-all-pagination') }}
             </div>
         </div>
-        {{-- MAIN CONTENT CONTAINER --}}
         <div class="container-right">
             <div class="card-display-view-all">
                 @foreach($deals as $deal)
