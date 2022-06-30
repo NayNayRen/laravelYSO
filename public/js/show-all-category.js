4ece930c4e5ae5fcf0f74b903893fb9d94687fea
Nate M <jeditendencies35@gmail.com>
2022-06-30 17:24:59 -0400

    moved a couple css styles around

--- public/js/show-all-category.js
+++ public/js/show-all-category.js
@@ -1,10 +1,8 @@
 const allButton = document.querySelector("#all-button");
+const filterSelectionDropdown = document.querySelector(
+    ".filter-selection-dropdown"
+);
 
 allButton.addEventListener("click", () => {
     console.log("clicked");
 });
-
-// allButton.addEventListener("click", () => {
-//     fashionContainer.style.display = "grid";
-//     foodContainer.style.display = "none";
-// });
