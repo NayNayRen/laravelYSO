c8c94ba093fc9e22bfcc32a72bad93e72c2a7473
Nate M <jeditendencies35@gmail.com>
2022-07-06 16:43:03 -0400

    added a setTimeout function to remove the search error after 5 seconds

--- public/js/show-search-error.js
+++ public/js/show-search-error.js
@@ -6,7 +6,14 @@ function showErrorMessage() {
         searchFormError.style.padding = "0";
     } else {
         // has an error message
+        searchFormError.style.opacity = 1;
         searchFormError.style.padding = "5px 0";
+        setTimeout(() => {
+            // error goes away after 5 seconds
+            searchFormError.style.opacity = 0;
+            searchFormError.innerText === "";
+            searchFormError.style.padding = "0";
+        }, 5000);
     }
 }
 
