d18b8504a01af84b88c47cf630d9e34426bca6a7
Nate M <jeditendencies35@gmail.com>
2022-07-07 22:37:08 -0400

    replaced/added the card component to all category pages

--- resources/views/category_pages/popular.blade.php
+++ resources/views/category_pages/popular.blade.php
@@ -85,33 +85,11 @@
             </div>
         </div>
         <div class="container-right">
+            {{-- CARD BLOCK --}}
             <div class="card-display-view-all">
                 @foreach($deals as $deal)
-                    {{-- CARD BLOCK --}}
-                    <div class="card">
-                        <div>
-                            <div class="card-logo-container">
-                                <img src="{{ $deal->picture_url }}" class="card-logo" alt="{{ $deal->name }}">
-                            </div>
-                            <span class="card-discount">{{ $deal->location }}</span><br>
-                            <span class="card-name">{{ $deal->name }}</span><br>
-                        </div>
-                        <div>
-                            <div class="views-likes-container">
-                                <div>
-                                    <span>Views: {{ $deal->views }}</span><br>
-                                    <span>Likes:</span>
-                                </div>
-                                <div class="views-likes-icons">
-                                    <i class="fa fa-share" aria-hidden="true"></i>
-                                    <i class="fa fa-star" aria-hidden="true"></i>
-                                </div>
-                            </div>
-                            <a href="/deals/{{ $deal->id }}">
-                                <div class="get-deal-button">Get Deal Now!</div>
-                            </a>
-                        </div>
-                    </div>
+                    {{-- CARD COMPONENT --}}
+                    @include('includes._card')
                 @endforeach
             </div>
         </div>
