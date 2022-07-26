54dacb8ce30c562a4a304f6e9fd753614c124a33
Nate M <jeditendencies35@gmail.com>
2022-07-26 12:20:21 -0400

    toying with the flash message

--- resources/views/includes/_flash_message_user.blade.php
+++ resources/views/includes/_flash_message_user.blade.php
@@ -1,7 +1,8 @@
 {{-- using alpine.js to remove the flash after 3 seconds --}}
 @if(session()->has('flash-message-user'))
     <div x-data="{show: true}" x-init="setTimeout(() => show = false, 5000)" x-show="show"
-        x-transition:leave.duration.400ms class="flash-message-user">
+        y-transition:leave.duration.400ms class="flash-message-user">
+        <img src="{{ asset('img/yso-logo2.svg') }}" class="yso-link" alt="Your Social Offers Logo">
         <p>{{ session('flash-message-user') }}</p>
     </div>
 @endif
