0d4a09c6725b1ae8da9200631da224544f7fad9f
Nate M <jeditendencies35@gmail.com>
2022-09-13 15:13:00 -0400

    cleaned up some commented out code

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -13,7 +13,6 @@ class CouponController extends Controller
 {
     public function userCoupons(Request $request)
     {
-        // return $request->all();
 
         if(auth()->user()){   
             $user_id = auth()->user()->id;
@@ -22,7 +21,6 @@ class CouponController extends Controller
                 if(isset($request->email)){
                     $check = UserCoupon::where('deal_id',$request->dealid)->where('user_id',$user_id)->first();
                     if($check != null && $check->email = $request->email){
-                        // $check->delete();
                         return response()->json([
                             'emailed-already' => 'Coupon Already Emailed.',
                         ]);
@@ -34,10 +32,8 @@ class CouponController extends Controller
                         $new->status = 0;
                         $new->email = $request->email;
                         $new->save();
-                        // return $deal;
+
                         $nxt =  Carbon::createFromFormat('Y-m-d H:i:s', $new->created_at);
-                        // return $nxt; 
-                        // return date_format($nxt->addDays(1),"Y-m-d H:i:s");
                         $data = array(
                             'subject' => 'Sending Coupon.',
                             'id' => $deal->id,
@@ -47,19 +43,16 @@ class CouponController extends Controller
                             'views' => $deal->views,
                             'expiry' => date_format($nxt->addDays(1),"Y-m-d H:i:s")
                         );
-                        // $email = 'shahzadanouman@hotmail.com';
                         Mail::to($request->email)->send(new CouponMail($data));
 
                         return response()->json([
                             'emailed' => 'Email Sent Successfully!',
-                            // 'message' =>$request->email,
                         ]);    
                     }    
                 }
                 if(isset($request->phone)){
                     $check = UserCoupon::where('deal_id',$request->dealid)->where('user_id',$user_id)->first();
                     if($check != null && $check->phone = $request->phone){
-                        // $check->delete();
                         return response()->json([
                             'texted-already' => 'Coupon Already Texted.',
                         ]);
@@ -71,68 +64,18 @@ class CouponController extends Controller
                         $new->status = 0;
                         $new->phone = $request->phone;
                         $new->save();
-                        // return $deal;
+
                         $nxt =  Carbon::createFromFormat('Y-m-d H:i:s', $new->created_at);
                         $route = route('deals.show',$deal->id);
                         $recipient = '+1    '.str_replace('-','',$request->phone);
                         $message_to_send = "Coupon Details \n".$deal->name."\n Click the link below to find details \n".$route. "\n Coupon will expire after 24 Hours at: ".$nxt;
         
                         $this->sendSms($recipient,$message_to_send);
-                        
-                        // //text ams starts here
-                        // $service_plan_id = env('SERVICE_PLAN_ID');
-                        // $bearer_token = env('BEARER_TOCKEN');
-
-                        // //Any phone number assigned to your API
-                        // $send_from = "+19512637122";
-                        // //May be several, separate with a comma ,
-                        // // +18135012075 
-                        // $recipient_phone_numbers = '+19'.str_replace('-','',$request->phone);
-                        // $route = route('deals.show',$deal->id);
-                        // // $message = "Click on the link bellow to Find You Coupon Details  {$recipient_phone_numbers} from {$send_from}";
-                        // $message = "Coupon Details \n".$deal->name."\n Click the link Bellow to find Dtials \n".$route. "\n Coupon will be Expired After 24 Hours at: ".$nxt;
-                        
-                        // // Check recipient_phone_numbers for multiple numbers and make it an array.
-                        // if(stristr($recipient_phone_numbers, ',')){
-                        // $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
-                        // }else{
-                        // $recipient_phone_numbers = [$recipient_phone_numbers];
-                        // }
-
-                        // // Set necessary fields to be JSON encoded
-                        // $content = [
-                        // 'to' => array_values($recipient_phone_numbers),
-                        // 'from' => $send_from,
-                        // 'body' => $message
-                        // ];
-
-                        // $data = json_encode($content);
-                        // $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
-                        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-                        // curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
-                        // curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
-                        // curl_setopt($ch, CURLOPT_POST, true);
-                        // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-                        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-                    
-                        // $result = curl_exec($ch);
-                        
-                        // // if(curl_errno($ch)) {
-                        // //     echo 'Curl error: ' . curl_error($ch);
-                        // // } else {
-                        // //     echo $result;
-                        // // }
-                        // curl_close($ch);
 
                         return response()->json([
                             'texted' => 'Text Sent Successfully!',
-                            // 'message' => 'phone is :'.$request->phone .$result,
-                            // 'message' =>$request->email,
                         ]);
                     }
-                // }
                 }
             }
         }else{
@@ -179,15 +122,7 @@ class CouponController extends Controller
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-    
         $result = curl_exec($ch);
-        
-        // if(curl_errno($ch)) {
-        //     echo 'Curl error: ' . curl_error($ch);
-        // } else {
-        //     echo $result;
-        // }
         curl_close($ch);
     }
 }
