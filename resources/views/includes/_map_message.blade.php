b6c31a4cbb75ca5e4b1857697664538388ea483f
Nate M <jeditendencies35@gmail.com>
2022-09-20 12:28:10 -0400

    forgot a little text

--- resources/views/includes/_map_message.blade.php
+++ resources/views/includes/_map_message.blade.php
@@ -4,7 +4,7 @@
     </div>
     <p>Oooops...</p>
     <p>Looks like there's no locations associated with your search.</p>
-    <p>That doesn't mean there's none available below though.</p>
+    <p>That doesn't mean there's none available though.</p>
     <div class="map-message-container">
         <img src="{{ asset('img/yso-logo2.svg') }}" class="yso-link" alt="Your Social Offers Logo">
     </div>
