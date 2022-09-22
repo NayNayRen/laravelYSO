43a73657bdf4354b069a042bde0522d3c25c1dcf
Nate M <jeditendencies35@gmail.com>
2022-09-22 02:06:07 -0400

    trying to get these markers to clear before placing again

--- public/js/show-map.js
+++ public/js/show-map.js
@@ -90,6 +90,7 @@ function loadScript() {
                     map: map,
                     shouldFocus: false,
                 });
+                // marker.setMap(null);
             });
         });
     }
