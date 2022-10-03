2bb150b898a72287fe287da59a792b9f6f7e2298
Nate M <jeditendencies35@gmail.com>
2022-10-02 21:52:47 -0400

    corrected spellings of favorites migration file

--- database/migrations/2022_07_04_081041_create_favorites_table.php
+++ database/migrations/2022_07_04_081041_create_favorites_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('favourites', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('deal_id')->nullable();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('favourites');
+        Schema::dropIfExists('favorites');
     }
 };
