133d1b83fc137c492588a1530d711e67f9f3f3bc
Nate M <jeditendencies35@gmail.com>
2022-12-12 13:35:49 -0500

    trying out some pagination stuff

--- resources/views/vendor/pagination/custom-view-all-pagination.blade.php
+++ resources/views/vendor/pagination/custom-view-all-pagination.blade.php
@@ -27,24 +27,35 @@ of
     {{-- ARRAY OF PAGE COUNTERS --}}
     @if(is_array($element))
         @foreach($element as $page => $url)
-            @if($page == $paginator->currentPage())
+
+            {{-- @if($paginator->currentPage() > 4 && $page === 2)
+                <span class="item disabled">...</span>
+@endif--}}
+
+@if($page == $paginator->currentPage())
                 <a class="item active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
-            @else
-                <a class="item" href="{{ $url }}">{{ $page }}</a>
-            @endif
-        @endforeach
-    @endif
+        @else
+            <a class="item" href="{{ $url }}">{{ $page }}</a>
+        @endif
+
+        {{-- @if($paginator->currentPage() < $paginator->lastPage()- 3 && $page === $paginator->lastPage() - 1)
+                <span class="item disabled">...</span>
+@endif--}}
+
+@endforeach
+@endif
 @endforeach
 </div>
 
 {{-- next button (right arrow) --}}
-@if($paginator->hasMorePages())
-    <a href="{{ $paginator->nextPageUrl() }}" class="view-all-right-arrow" aria-label="Next Deal"><i
-            class="fa fa-arrow-right" aria-hidden="false"></i>
-    </a>
-@else
-    <span class="view-all-right-arrow" aria-label="Next Deal"><i class="fa fa-arrow-right" aria-hidden="false"></i>
-    </span>
-@endif
+        @if($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" class="view-all-right-arrow" aria-label="Next Deal"><i
+                    class="fa fa-arrow-right" aria-hidden="false"></i>
+            </a>
+        @else
+            <span class="view-all-right-arrow" aria-label="Next Deal"><i class="fa fa-arrow-right"
+                    aria-hidden="false"></i>
+            </span>
+        @endif
 
-@endif
+    @endif
