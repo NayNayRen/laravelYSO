4b6408ea2fc92c2b85d4b9d9666e489719f7ba42
Nate M <jeditendencies35@gmail.com>
2022-12-14 16:21:07 -0500

    worked out pagination with dots, beautify blade was causing weird characters

--- resources/views/vendor/pagination/custom-view-all-pagination.blade.php
+++ resources/views/vendor/pagination/custom-view-all-pagination.blade.php
@@ -11,28 +11,36 @@
         </a>
     @endif
 
-    {{-- item count --}}
+    {{-- item pagination --}}
     <div class="view-all-item-counter">
-        {{-- @if($paginator->firstItem())
-            <span>{{ $paginator->firstItem() }}</span>
-        to
-        <span>{{ $paginator->lastItem() }}</span>
-    @else
-        {{ $paginator->count() }}
-@endif
-of
-<span>{{ $paginator->total() }}</span> --}}
 
 @foreach($elements as $element)
     {{-- ARRAY OF PAGE COUNTERS --}}
     @if(is_array($element))
         @foreach($element as $page => $url)
 
-            @if($page == $paginator->currentPage())
+        @if ($paginator->currentPage() > 4 && $page === 2)
+            <span class="disabled">...</span>
+        @endif
+
+            {{-- @if($page == $paginator->currentPage())
                 <a class="item active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
             @else
                 <a class="item" href="{{ $url }}">{{ $page }}</a>
+            @endif --}}
+
+            {{-- REMOVE IF NOT LIKED --}}
+            @if($page == $paginator->currentPage())
+                <a class="item active" href="{{ $url }}" aria-current="page">{{ $page }}</a>
+            {{-- @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() + 2 || $page === $paginator->currentPage() - 1 || $page === $paginator->currentPage() - 2 || $page === $paginator->lastPage() || $page === 1) --}}
+            @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() - 1 || $page === $paginator->lastPage() || $page === 1)
+                <a class="item" href="{{ $url }}">{{ $page }}</a>
             @endif
+
+        @if ($paginator->currentPage() < $paginator->lastPage() - 3 && $page === $paginator->lastPage() - 1)
+            <span class="disabled">...</span>
+        @endif
+        {{-- REMOVE IF NOT LIKED --}}
 
         @endforeach
     @endif
