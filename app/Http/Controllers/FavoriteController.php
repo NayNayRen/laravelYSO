75e47927fb6b8a1a640fd14850a4f43c65ba7b6f
Nate <jeditendencies35@gmail.com>
2022-12-27 23:14:11 -0500

    formatted by blade formatter

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -7,35 +7,34 @@ use App\Models\Favorite;
 
 class FavoriteController extends Controller
 {
-    public function favoriteDeal(Request $request)  
+    public function favoriteDeal(Request $request)
     {
         // dd ($request->all());
-        if(auth()->user()){   
+        if (auth()->user()) {
             $user_id = auth()->user()->id;
-            
-            if ($request->ajax()){
 
-                $check = Favorite::where('deal_id',$request->id)->where('user_id',$user_id)->first();
-                if($check != null){
-                    $check->delete();    
+            if ($request->ajax()) {
+
+                $check = Favorite::where('deal_id', $request->id)->where('user_id', $user_id)->first();
+                if ($check != null) {
+                    $check->delete();
                     return response()->json([
-                        'delete' =>'Removed from Favorites list!',
+                        'delete' => 'Removed from Favorites list!',
                     ]);
-                }else{
+                } else {
                     $new  = new Favorite;
                     $new->deal_id = $request->id;
                     $new->user_id = $user_id;
                     $new->save();
                     return response()->json([
-                        'success' =>'Added to Favorites successfully!',
+                        'success' => 'Added to Favorites successfully!',
                     ]);
                 }
             }
-        }else{
+        } else {
             return response()->json([
-                'error' =>'Please login first!',
+                'error' => 'Please login first!',
             ]);
         }
-        
     }
 }
