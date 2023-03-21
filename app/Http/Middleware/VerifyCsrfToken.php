611e2c1903283f3c40762759716ba4028662ea08
Nate <jeditendencies35@gmail.com>
2023-03-20 22:31:52 -0400

    added apple callback to csrf token middleware file

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // added for Apple callback in user log in
+        'auth/apple/callback'
     ];
 }
